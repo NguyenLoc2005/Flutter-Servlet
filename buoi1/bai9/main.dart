c8aa65925b1371cc90832ebb6e3d00b23a57069a
NguyenLoc2005 <nxl522005@gmail.com>
2025-08-28 14:43:02 +0700

    updated

--- buoi1/bai9/main.dart
+++ buoi1/bai9/main.dart
@@ -1,3 +1,31 @@
+import 'dart:io';
+
+import 'student.dart';
+
 void main(){
-  print('Hi Im Humble Kind');
+  List<Student> listStudent =[];
+  int chon;
+
+  do{
+    print('Hi Im Humble Kind');
+    print('1. Nhập dữ liệu');
+    print('2. Hiển thị dữ liệu');
+    print('3. Xem bảng xếp hạng');
+    stdout.write('Bạn chọn chức năng nào: ');
+    chon = int.parse(stdin.readLineSync()!);
+
+
+    if(chon==1){
+      Student student =  Student("", "", "", 0, "", "", 0, 0);
+      student.Nhap();
+      listStudent.add(student);
+    }
+    else if(chon==2){
+      for(var i in listStudent){
+        i.HienThi();
+      }
+    }
+  }
+  while(chon!=0);
+
 }
